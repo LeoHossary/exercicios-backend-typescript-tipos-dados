a1bb3097d53c54bb9416af1b7dd79de00408d7bc
LeoHossary <leonardohossary@gmail.com>
2024-01-19 20:13:05 -0400

    refactor: adicionada a solução do professor

--- 05/index.ts
+++ 05/index.ts
@@ -1,13 +1,17 @@
 const soletrar = (palavra: string): string => {
-  let resultado: string = "";
-  for (let index = 0; index < palavra.length; index++) {
-    resultado += `${palavra[index]}`;
+  //SOLUÇÃO DO PROFESSOR
+  return palavra.split("").join("-");
 
-    if (index !== palavra.length - 1) {
-      resultado += "-";
-    }
-  }
-  return resultado;
+  //------>SOLUÇÃO PENSADA PRIMEIRO
+  // let resultado: string = "";
+  // for (let index = 0; index < palavra.length; index++) {
+  //   resultado += `${palavra[index]}`;
+
+  //   if (index !== palavra.length - 1) {
+  //     resultado += "-";
+  //   }
+  // }
+  // return resultado;
 };
 
 console.log(soletrar("programador"));
